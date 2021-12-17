f8e1e3ef4b148251a4fa84586a11ed93d5fa46ee
Developer <gmk.hussain@gmail.com>
2021-12-17 13:58:03 +0500

    Line Chart

--- src/views/frontend/pages/HomePage.vue
+++ src/views/frontend/pages/HomePage.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="hello">
     Home
+    <LineChart :data="[99, 71, 78, 25, 36, 92]" />
   </div>
 </template>
 
 <script>
+import LineChart from './ChartsPage.vue'
 export default {
   name: 'HomePage',
+  components: {
+    LineChart
+  },
   props: {
     msg: String
   }
