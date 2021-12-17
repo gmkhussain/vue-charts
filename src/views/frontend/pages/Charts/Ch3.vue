f3c25f4912bffdc59b3057c7aaf4fd59bd3a22ba
Developer <gmk.hussain@gmail.com>
2021-12-17 17:52:06 +0500

    Function of Data

--- src/views/frontend/pages/Charts/Ch3.vue
+++ src/views/frontend/pages/Charts/Ch3.vue
@@ -4,6 +4,10 @@
     <p></p>
     <p></p>
     <p></p>
+
+    <p>Error: This is error.</p>
+    <p>Warning:This is warning.</p>
+
   </div>
 </template>
 
@@ -13,6 +17,9 @@ import * as d3 from 'd3'
 export default {
     name: "Ch3",
     mounted() {
+
+
+        // Function of Data
         var data = [100, 200, 300];
         var paragraph = d3.select("body")
                 .selectAll("p")
@@ -26,6 +33,23 @@ export default {
                 });
 
             console.log(paragraph);
+
+
+
+        // Dynamic Properties
+        d3.selectAll("p").style("color", function(d, i) {
+            var text = this.innerText;
+            
+            console.log(i); // i never used..
+
+            if (text.indexOf("Error") >= 0) {
+                return "red";
+            } else if (text.indexOf("Warning") >= 0) {
+                return "yellow";
+            }
+        });
+
+
     }
 }
 </script>
